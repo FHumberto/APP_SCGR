d76fa546e0f79fd4dbdb7f1ba168cbfaf902a953
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2025-06-20 16:05:47 -0300

    add: abstrações de requisição para data e paginação

---
+++ src/SCGR.Application/Common/Params/DateParams.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SCGR.Application.Common.Params;
+
+/// <summary>
+/// Representa os parâmetros de intervalo de datas para consultas.
+/// </summary>
+public class DateParams
+{
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+    [DefaultValue(typeof(DateOnly), $"2025-01-01")]
+    public required DateOnly StartDate { get; set; } = DateOnly.MinValue;
+
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+    [DefaultValue(typeof(DateOnly), "9999-12-31")]
+    public required DateOnly EndDate { get; set; } = DateOnly.MaxValue;
+}
---
+++ src/SCGR.Application/Common/Params/PaginationParams.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace SCGR.Application.Common.Params;
+
+/// <summary>
+/// Representa os parâmetros de paginação para consultas.
+/// </summary>
+public class PaginationParams
+{
+    [DefaultValue(1)]
+    public required int PageNumber { get; set; } = 1;
+    [DefaultValue(10)]
+    public required int PageSize { get; set; } = 10;
+}
